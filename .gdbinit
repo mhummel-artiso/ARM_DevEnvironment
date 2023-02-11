84c64af3f4f80625b07f3e1ccd4fb8108d0b3702
mhummel-artiso <73578966+mhummel-artiso@users.noreply.github.com>
2023-02-11 18:02:42 +0000

    try next configuration

---
+++ .gdbinit
@@ -0,0 +1,7 @@
+target remote localhost:1234
+set architecture armv3
+layout r
+
+# define hook-quit
+# 	set confirm off
+# end
--- compileAndDebugOnARM.sh
+++ compileAndDebugOnARM.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-#set -eo pipefail
+#!/bin/bash
+set -eo pipefail
 
 FILEPATH=$(echo "$1" | awk -F'.' '{print $1}')
 DEBUG_PORT=1234
@@ -22,17 +22,19 @@ mkdir -p DIR
 
 # compile
 echo "compiling $FILENAME ..."
-arm-none-eabi-as ./$FILEPATH.S -g -o "./bin/$FILEPATH.el" 
-# | echo "compiling failed!"
+arm-none-eabi-as "./$FILEPATH.S" -g -o "./bin/$FILEPATH.o"  | echo "compiling failed!"
 
 # link
 echo "linking $FILENAME ..."
-arm-none-eabi-ld ./bin/$FILEPATH.el -o "./bin/$FILEPATH.o" 
-# | echo "linking failed!"
+arm-none-eabi-ld "./bin/$FILEPATH.o" -o "./bin/$FILEPATH.elf"  | echo "linking failed!"
+
+chmod +x "./bin/$FILEPATH.elf"
+
+# gdb --args "./bin/$FILEPATH.elf"
 
 # start binary in qemu arm emulator
 echo "started ARM Emulation on port $port"
-qemu-arm -singlestep -g $DEBUG_PORT "./bin/$FILEPATH.o"
+qemu-arm -singlestep -g $DEBUG_PORT "./bin/$FILEPATH.elf"
 
 # start gnu debugger
-gdb-multiarch "./bin/$FILEPATH.o"
+gdb-multiarch "./bin/$FILEPATH.elf"
